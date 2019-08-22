de1ccfeac225067f4a452d5f3abf115d02325e58
nilotpalgit <devworks.bk@gmail.com>
2019-08-22 20:35:33 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com/?q=Nilotpal"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Nilotpal
         </a>
       </header>
     </div>
